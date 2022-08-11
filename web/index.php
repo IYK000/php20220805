ce06136ea7703a1810a1218caa752a35e45e82c5
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 11:34:23 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ try {
 
 	// debug
 	echo '<pre>';
-	echo var_dump(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
+	echo var_dump("aa");
 	echo '</pre>';
 ?>
 <!DOCTYPE html>
