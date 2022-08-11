ee9b0e827418c0208f8c6bafabea00dc3803ee7f
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 12:41:17 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -18,7 +18,7 @@
 
 	// debug
 	echo '<pre>';
-	echo var_dump(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
+	echo var_dump(parse_url("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
 	echo '</pre>';
 ?>
 <!DOCTYPE html>
