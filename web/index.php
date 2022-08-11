96aa178bdbde2c2b8da487027bc5cbcc7b389c52
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 11:10:47 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+try {
 	$db = parse_url(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
 
 	$pdo = new PDO("pgsql:" . sprintf(
@@ -9,16 +10,20 @@
 		$db["pass"],
 		ltrim($db["path"], "/")
 	));
+	} catch (PDOException $e) {
+		$isConnect = false;
+		$msg       = "MySQL への接続に失敗しました。<br>(" . $e->getMessage() . ")";
+	}
 ?>
 <!DOCTYPE html>
 <html lang="jp">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
 <body>
-
+	<p><?php echo $msg; ?></p>
 </body>
 </html>
