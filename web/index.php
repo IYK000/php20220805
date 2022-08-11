ef7fabd9951174c8b8ef903d261cbaab875ac495
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 11:19:37 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,18 @@
 <?php
 try {
+	define("PG_HOST","ec2-54-85-56-210.compute-1.amazonaws.com");
+	define("PG_PORT", "5432");
+	define("PG_USER", "eyencudwuyhxpn");
+	define("PG_PASS", "caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb");
+
 	$db = parse_url(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
 
 	$pdo = new PDO("pgsql:" . sprintf(
 		"host=%s;port=%s;user=%s;password=%s;dbname=%s",
-		$db["host"],
-		$db["port"],
-		$db["user"],
-		$db["pass"],
+		PG_HOST,
+		PG_PORT,
+		PG_USER,
+		PG_PASS,
 		ltrim($db["path"], "/")
 	));
 	} catch (PDOException $e) {
