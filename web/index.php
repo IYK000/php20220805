94192d4bd5365ff70318cf0d0a9e1cbc2b67579b
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 11:09:04 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,15 @@
+<?php
+	$db = parse_url(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
 
+	$pdo = new PDO("pgsql:" . sprintf(
+		"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+		$db["host"],
+		$db["port"],
+		$db["user"],
+		$db["pass"],
+		ltrim($db["path"], "/")
+	));
+?>
 <!DOCTYPE html>
 <html lang="jp">
 <head>
@@ -8,8 +19,6 @@
   <title>Document</title>
 </head>
 <body>
-<?php 
-  echo 'hello world!';
-?>
+
 </body>
 </html>
