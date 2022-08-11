fdac79c955daab63847bec9f1e43b31c345a25b0
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 13:00:40 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,10 +1,10 @@
 <?php
 	try {
-		$db = parse_url(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
+		$db = parse_url("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c");
 		
 
 		$pdo = new PDO("pgsql:" . sprintf(
-			"host=ec2-54-85-56-210.compute-1.amazonaws.com;port=5432;user=eyencudwuyhxpn;password=caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb;dbname=d28e45qo92r39c",
+			"host=%s;port=%s;user=%s;password=%s;dbname=%s",
 			$db["host"],
 			$db["port"],
 			$db["user"],
@@ -13,12 +13,13 @@
 	));
 	} catch (PDOException $e) {
 			$isConnect = false;
-			$msg       = "接続に失敗しました。<br>(" . $e->getMessage() . ")";
+			$msg       = "DB接続に失敗しました。<br>(" . $e->getMessage() . ")";
+			exit();
 	}
 
 	// debug
 	echo '<pre>';
-	echo var_dump(parse_url("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
+	echo var_dump();
 	echo '</pre>';
 ?>
 <!DOCTYPE html>
