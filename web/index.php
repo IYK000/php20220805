c7c12edea25f7bec272cc11a743f1513ac6399c0
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 11:28:22 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ try {
 	$db = parse_url(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
 
 	$pdo = new PDO("pgsql:" . sprintf(
-		"host=ec2-54-85-56-210.compute-1.amazonaws.com;port=5432;user=eyencudwuyhxpn;password=caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb;dbname=d28e45qo92r39c",
+		"host=PG_HOST;port=5432;user=eyencudwuyhxpn;password=caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb;dbname=d28e45qo92r39c",
 		PG_HOST,
 		PG_PORT,
 		PG_USER,
