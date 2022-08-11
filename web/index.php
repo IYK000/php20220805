d21fa5dbe0f7c883c415abed2e7fd9cb88343309
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-11 11:33:23 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,18 +4,18 @@ try {
 	//$db = parse_url(getenv("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c"));
 	
 
-	$pdo = new PDO("pgsql:" . sprintf(
-		"host=ec2-54-85-56-210.compute-1.amazonaws.com;port=5432;user=eyencudwuyhxpn;password=caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb;dbname=d28e45qo92r39c",
-		PG_HOST,
-		PG_PORT,
-		PG_USER,
-		PG_PASS,
-		PG_DB
-	));
-	} catch (PDOException $e) {
-		$isConnect = false;
-		$msg       = "接続に失敗しました。<br>(" . $e->getMessage() . ")";
-	}
+	// $pdo = new PDO("pgsql:" . sprintf(
+	// 	"host=ec2-54-85-56-210.compute-1.amazonaws.com;port=5432;user=eyencudwuyhxpn;password=caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb;dbname=d28e45qo92r39c",
+	// 	PG_HOST,
+	// 	PG_PORT,
+	// 	PG_USER,
+	// 	PG_PASS,
+	// 	PG_DB
+	// ));
+	// } catch (PDOException $e) {
+	// 	$isConnect = false;
+	// 	$msg       = "接続に失敗しました。<br>(" . $e->getMessage() . ")";
+	// }
 
 	// debug
 	echo '<pre>';
