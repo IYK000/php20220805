661a6d0eb7396de7c30d18c1e999adcf0ae035bc
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-15 17:48:37 +0900

    test

---
+++ common/db.php
@@ -0,0 +1,3 @@
+<?php
+	define("DB_URL", "postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c");
+?>
--- web/main.php
+++ web/main.php
@@ -1,9 +1,7 @@
 <?php
-	// 
-	require_once(dirname(__FILE__) . "/../db.php");
-
 	try {
-		$db = parse_url(DB_URL);
+		$db = parse_url("postgres://eyencudwuyhxpn:caea2aebea0daf832a47e3de0df1d4d349fb39780918cbd946a8ddfb3ab25fdb@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d28e45qo92r39c");
+		
 
 		$pdo = new PDO("pgsql:" . sprintf(
 			"host=%s;port=%s;user=%s;password=%s;dbname=%s",
@@ -17,11 +15,15 @@
 			$isConnect = false;
 			$msg       = "DB接続に失敗しました。<br>(" . $e->getMessage() . ")";
 	}
+
+	// SQL
+	$stmt = $pdo->query("UPDATE salesforce.account SET memo_all__c='メモ' WHERE id=1;");
+	$rs = $stmt->fetchall();
  
-	// debug
-	echo '<pre>';
-	echo var_dump(DB_URL);
-	echo '</pre>';
+	// // debug
+	// echo '<pre>';
+	// echo var_dump($rs);
+	// echo '</pre>';
 ?>
 <!DOCTYPE html>
 <html lang="jp">
