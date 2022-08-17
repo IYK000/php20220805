d346f14d83df0943b0be281f622b95eabb87d2ae
k-iriyama <60993673+IYK000@users.noreply.github.com>
2022-08-17 11:30:23 +0900

    Update main.php

--- web/main.php
+++ web/main.php
@@ -1,6 +1,6 @@
 <?php
 	// 外部ファイル取込み
-	require_once(dirname(__FILE__) . '/../class_db/loginDb.php');
+	// require_once(dirname(__FILE__) . '/../class_db/loginDb.php');
 
 	// $MemberId = $_POST['MemberId'];
 	// $LoginPassword = $_POST['LoginPassword'];
